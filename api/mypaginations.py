3cfed2471c0aa532b687ba0bae15a78f3cc8fc09
“[firstname <133085428+sh-17@users.noreply.github.com>
2023-12-07 14:22:41 +0530

    new

--- api/mypaginations.py
+++ api/mypaginations.py
@@ -2,9 +2,9 @@ from rest_framework.pagination import LimitOffsetPagination
 
 
 class MyLimitOffsetPagination(LimitOffsetPagination):
-    default_limit = 35
+    default_limit = 20
     limit_query_param = 'limit'
     offset_query_param = 'offset'
-    max_limit = 1
+    max_limit = 3
 
 # http://127.0.0.1:8000/offerapi/?limit=1&offset=2
